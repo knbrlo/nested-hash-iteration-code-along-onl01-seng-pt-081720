fee0a01c484db5f073487d6b1c80a9616f72af69
Ken Barlow <kenbrlow@gmail.com>
2020-08-30 01:17:44 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,7 +16,9 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    puts person
     if person == "Freddy Mercury"
+      puts contact_details_hash
       contact_details_hash.each do |attribute, data|
         if attribute == :favorite_ice_cream_flavors
           data.delete_if {|ice_cream| ice_cream == "strwberry"}
